d8f8ef8c3e3b108c083025181373f39790a78322
HARMANJOT SINGH NANDA <hxrmn03@gmail.com>
2025-05-21 16:28:49 +0530

    post complaints done

--- controllers/compcon.js
+++ controllers/compcon.js
@@ -1,11 +1,37 @@
 const pool = require('../db')
 
 
-const postcomplaint=async (req,res)=>{
+const postcomplaint=async (req,res)=>{//for students to post comp
+    let{sid, complaint,photo}=req.body
+    let ip = req.ip
+    try {
+        if(!sid||!complaint){
+            return res.json({
+                message:"invalid request"
+            })
+        }
+
+        let createdat= new Date()
+        const query = "INSERT INTO complaints (sid,complaint,created_at,ip,photo) Values(?,?,?,?,?)"
+        const [result] = await pool.execute(query,[sid,complaint,createdat,ip,photo])
+        const comp_id = result.insertId
+        // console.log(result)
+        return res.json({
+            message:"complaint registered succesfully",
+            complaintid : comp_id
+        })
+    } catch (error) {
+        return res.json({
+            message:error.message
+        })
+    }
+
+
+
 
 }
 
-const getcomplaints = async (req,res)=>{
+const getcomplaints = async (req,res)=>{ // for admin to see all complaints
     console.log(req.user)
     return res.json({
         message:"welcome to protected route",
